1e03edfb3c50e351f346d606e3f12f0bd078f354
Lim Sung Hyun <sh.lim+github@woowahan.com>
2019-02-03 19:21:13 +0900

    1 Getting Started

--- flaskblog.py
+++ flaskblog.py
@@ -1,30 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask
 app = Flask(__name__)
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return "<h1>home</h1>"
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return "<h1>About</h1>"
 
 
 if __name__ == '__main__':
