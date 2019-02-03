46f812caea4877015a4758f539804b3b9f2be2de
Lim Sung Hyun <sh.lim+github@woowahan.com>
2019-02-03 19:26:45 +0900

    2.render_template 추가

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>home</h1>"
+    return render_template('home.html')
 
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
