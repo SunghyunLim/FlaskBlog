b9623a8bf1a409b646bb8ba68e6f031e391d6246
Lim Sung Hyun <sh.lim+github@woowahan.com>
2019-02-03 18:20:52 +0900

    2.Template 첫 적용

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html')
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
